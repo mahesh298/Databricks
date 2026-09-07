f19884c449198d55754befa4348c5469075ea2db
maheshsompalli2@gmail.com <maheshsompalli2@gmail.com>
2026-09-07 00:55:37 +0000

    Ran the notebook

--- databricks-course-v1/databricks-course/introduction-to-notebooks/1.Notebooks Introduction.py
+++ databricks-course-v1/databricks-course/introduction-to-notebooks/1.Notebooks Introduction.py
@@ -1,4 +1,8 @@
 # Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
 # MAGIC %md
 # MAGIC # Introduction to Notebooks
 # MAGIC # Heading 1
